eda4ea5c410ec15d9a757ab1dba18bac2917b286
rutraq <a3332133@gmail.com>
2019-11-28 00:37:27 +0300

    check with pages

--- check.py
+++ check.py
@@ -1,5 +1,14 @@
 import requests
 from lxml import html
+import re
+
+
+def get_pages(url):
+    r = requests.get(url + '1')
+    tree = html.fromstring(r.content)
+    pages = tree.xpath('//*[@id="content"]/div[3]/div[2]')[0].text
+    pages = re.search('\d+ ст', pages)
+    return int(pages[0][:-3])
 
 
 def check(pages, url):
@@ -17,5 +26,5 @@ def check(pages, url):
 
 
 if __name__ == "__main__":
-    url_enter = "https://zozo.by/tovary-dlya-doma-i-sada/sadovaya-tehnika-osnastka-i-prinadlezhnosti/benzopily-i-pily-cepnye-elektricheskie?limit=100&page="
-    check(4, url_enter)
+    url_enter = "https://zozo.by/tovary-dlya-doma-i-sada/sadovaya-tehnika-osnastka-i-prinadlezhnosti/nasosy-i-nasosnye-stancii?limit=100&page="
+    check(get_pages(url_enter), url_enter)
